95b393dda8f748fe378ebbb1c25fb3d816cc8346
MeqdadDev <meqdad.darweesh@gmail.com>
2023-09-04 22:21:24 +0300

    implement operations on linkedlistStack

--- java/stack/LinkedListStack/Main.java
+++ java/stack/LinkedListStack/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Stack s = new Stack();
+        NodeData d1 = new NodeData(10);
+        NodeData d2 = new NodeData(20);
+
+        s.push(d1);
+        System.out.println(s.top());
+        s.push(d2);
+        System.out.println(s.top());
+        s.pop();
+        System.out.println(s.top());
     }
 }
